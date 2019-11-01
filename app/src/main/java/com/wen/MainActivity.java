9d4eca0fad2468e4171e131751267e90de90a221
wupeiwen <wendy89711@gmail.com>
2019-11-01 14:52:56 +0800

    user name 字數<3顯示錯誤成功； 顯示註冊成功頁面。

--- app/src/main/java/com/wen/MainActivity.java
+++ app/src/main/java/com/wen/MainActivity.java
@@ -1,26 +1,39 @@
 package com.wen;
 
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView name;
+    private TextView mail;
+    private TextView pass;
+    private ImageView errorName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        startActivity(new Intent(this, TestActivity.class));
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        name = findViewById(R.id.ed_username);
+        mail = findViewById(R.id.ed_email);
+        pass = findViewById(R.id.ed_password);
+        errorName = findViewById(R.id.imageView_name);
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,6 +42,22 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    @SuppressLint("WrongConstant")
+    public void registerButton(View view) {
+        name.getText();
+        pass.getText();
+        if (name.length() >= 3 && pass.length() >= 6) {
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Registration Success")
+                    .setMessage("Welcome")
+                    .setPositiveButton("OK", null)
+                    .show();
+        }else if (name.length() < 3) {
+            errorName.setVisibility(View.VISIBLE);
+
+        }
     }
 
     @Override
---
+++ app/src/main/java/com/wen/TestActivity.java
@@ -0,0 +1,4 @@
+package com.wen;
+
+class TestActivity {
+}
